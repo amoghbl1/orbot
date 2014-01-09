85c24e9906ac368cecb4dcf569b433a0d9155bb9
amoghbl1 <amoghbl1@gmail.com>
2014-01-09 21:01:08 +0530

    more changes made to the activity

--- src/org/torproject/android/service/NotificationHandler.java
+++ src/org/torproject/android/service/NotificationHandler.java
@@ -11,6 +11,6 @@ public class NotificationHandler extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.layout_main);
+		
 	}
 }
