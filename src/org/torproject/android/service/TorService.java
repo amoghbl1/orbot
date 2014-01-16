e2e95f17321eea6e64e357a4ff3fa1b5e4025803
amoghbl1 <amoghbl1@gmail.com>
2014-01-16 11:01:52 +0530

    expanded notification with remote view testing and corresponding code commented out and added in

--- src/org/torproject/android/service/TorService.java
+++ src/org/torproject/android/service/TorService.java
@@ -203,6 +203,8 @@ public class TorService extends Service implements TorServiceConstants, TorConst
  	{
  		//This is the remote view that will be used for the notification
 		RemoteViews notificationView = new RemoteViews(getPackageName(),R.layout.layout_notification);
+		// Notification to be used if remote expanded view is used.
+		Notification notification;
 		
 		if (mNotifyBuilder == null)
 		{
@@ -239,11 +241,18 @@ public class TorService extends Service implements TorServiceConstants, TorConst
 		
 		mNotifyBuilder.setContent(notificationView);
 		
+		notification = mNotifyBuilder.build();
+		notification.bigContentView = notificationView;
+		
 		mNotificationManager.notify(
+					notifyId,
+					notification);
+		
+		/*mNotificationManager.notify(
 					notifyId,
 	    			mNotifyBuilder.build());
 			
-		
+		*/
  	}
     
     /* (non-Javadoc)
