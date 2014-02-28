980d91769f6b64d7753c57c2b551d5a031832468
amoghbl1 <amoghbl1@gmail.com>
2014-03-01 02:31:42 +0530

    clean ChooseLocaleWizardActivity

--- src/org/torproject/android/wizard/ChooseLocaleWizardActivity.java
+++ src/org/torproject/android/wizard/ChooseLocaleWizardActivity.java
@@ -77,6 +77,7 @@ public class ChooseLocaleWizardActivity extends Activity implements TorConstants
 						int arg2, long arg3) {
 					
 					setLocalePref(arg2);
+					finish();
 					startActivityForResult(new Intent(getBaseContext(), LotsaText.class), 1);
 					
 				}
@@ -85,8 +86,7 @@ public class ChooseLocaleWizardActivity extends Activity implements TorConstants
 		    next.setOnClickListener(new View.OnClickListener() {
 				
 				public void onClick(View v) {
-					
-					
+					finish();
 					startActivityForResult(new Intent(getBaseContext(), LotsaText.class), 1);
 
 				}
